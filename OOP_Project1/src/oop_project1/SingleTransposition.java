19d26ab4308c3e0b5f98d1a171054109668e2365
Mehmet Anıl TAYSİ <36363193+MehmetAnil@users.noreply.github.com>
2021-01-02 20:39:03 +0300

    Add files via upload

---
+++ OOP_Project1/src/oop_project1/SingleTransposition.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop_project1;
+
+/**
+ *
+ * @author Mehmet Anıl TAYSİ
+ */
+public class SingleTransposition {
+    
+    
+    public static void findSingleTransposition(String word, String wordD){
+        
+       
+    }
+    
+}
